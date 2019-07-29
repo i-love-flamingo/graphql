dd14562d07885164dd210f946d23f31ffba2a54c
Leo Back <leo.back@aoe.com>
2019-07-29 17:02:22 +0200

    [Task] #203233 - Write & test mutation example

--- cors/corshandler.go
+++ cors/corshandler.go
@@ -12,6 +12,8 @@ func (h *Handler) validateOrigin(origin string) bool {
 	for _, whitelistEntry := range h.Whitelist {
 		if whitelistEntry == origin {
 			return true
+		} else if whitelistEntry == "*" {
+			return true
 		}
 	}
 	return false
@@ -20,17 +22,15 @@ func (h *Handler) validateOrigin(origin string) bool {
 func (h *Handler) addCorsHeaders(writer http.ResponseWriter, r *http.Request) {
 	origin := r.Header.Get("Origin")
 	if h.validateOrigin(origin) {
-		writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		writer.Header().Set("Access-Control-Allow-Origin", origin)
 		writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		writer.Header().Set("Access-Control-Allow-Methods", "POST")
-		writer.Header().Set("Access-Control-Allow-Headers", "content-type")
+		writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	}
 }
 
 func (h *Handler) PreflightHandler() http.Handler {
-	return http.HandlerFunc(func(writer http.ResponseWriter, r *http.Request) {
-		h.addCorsHeaders(writer, r)
-	})
+	return http.HandlerFunc(h.addCorsHeaders)
 }
 
 func (h *Handler) GqlMiddleware(gqlHandler http.Handler) http.Handler {

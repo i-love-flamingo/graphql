1b12849498ba07474b3fdc1eba2d39c1f9c55881
Stanislav Müller <stanislav.mueller@aoe.com>
2019-07-29 13:20:25 +0200

    T#203768 Align product attributes exports

--- helper_test.go
+++ helper_test.go
@@ -36,5 +36,24 @@ func TestHelperModels(t *testing.T) {
 	if TestModelFieldsTestMethod.FieldName != "TestModelMethod" {
 		t.Error("Test Model method field name is not matching")
 	}
+}
 
+func TestHelperModelsFlat(t *testing.T) {
+	res := ModelMap{
+		"TestModel": TestModel{},
+	}.Models()
+
+	if _, ok := res["TestModel"]; !ok {
+		t.Error("Test Model is not implemented")
+	}
+
+	TestModelValue := res["TestModel"]
+
+	if TestModelValue.Model.Has("flamingo.me/graphql.TestModel") == false {
+		t.Error("Test Model not mapped correctly")
+	}
+
+	if TestModelValue.Fields != nil {
+		t.Error("Test Model Fields are not empty")
+	}
 }

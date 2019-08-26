0d9638fee0b85aa055dc1dae377938ae0c6d33a6
Bastian Ike <bastian.ike@aoe.com>
2019-08-26 16:02:53 +0200

    clean, use flamingo v3

--- example/main.go
+++ example/main.go
@@ -4,14 +4,9 @@ package main
 //go:generate go run -tags graphql main.go graphql
 
 import (
-	"bytes"
-	"context"
-	"io/ioutil"
-
 	"flamingo.me/dingo"
 	"flamingo.me/flamingo/v3"
 	"flamingo.me/flamingo/v3/core/requestlogger"
-	"flamingo.me/flamingo/v3/framework/web"
 	projectgraphql "flamingo.me/graphql/example/graphql"
 	"flamingo.me/graphql/example/todo"
 )
@@ -21,23 +16,5 @@ func main() {
 		new(todo.Module),
 		new(projectgraphql.Module),
 		new(requestlogger.Module),
-		new(module),
 	})
-}
-
-type module struct{}
-
-func (*module) Configure(injector *dingo.Injector) {
-	web.BindRoutes(injector, new(routes))
-}
-
-type routes struct{}
-
-func (*routes) Routes(registry *web.RouterRegistry) {
-	registry.HandleAny("static", filehandler)
-}
-
-func filehandler(ctx context.Context, r *web.Request) web.Result {
-	b, _ := ioutil.ReadFile(r.Params["file"])
-	return &web.Response{Status: 200, Body: bytes.NewReader(b)}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module flamingo.me/graphql
 go 1.12
 
 require (
-	flamingo.me/dingo v0.1.6-0.20190724094148-8f9f5b4d8d0f
-	flamingo.me/flamingo/v3 v3.0.0-beta.3.0.20190822153000-cbfe37613b9b
+	flamingo.me/dingo v0.1.6
+	flamingo.me/flamingo/v3 v3.0.0
 	github.com/99designs/gqlgen v0.9.0
 	github.com/go-bindata/go-bindata v3.1.2+incompatible // indirect
 	github.com/spf13/cobra v0.0.3

fb7aee8172352af76c1b92719d94bf86f18b1a4c
Carsten Dietrich <carsten.dietrich@omnevo.net>
2023-07-04 09:23:30 +0200

    chore: readd missing project graphql module

--- example/main.go
+++ example/main.go
@@ -7,12 +7,14 @@ import (
 	"flamingo.me/dingo"
 	"flamingo.me/flamingo/v3"
 	"flamingo.me/flamingo/v3/core/requestlogger"
+	projectgraphql "flamingo.me/graphql/example/graphql"
 	"flamingo.me/graphql/example/todo"
 )
 
 func main() {
 	flamingo.App([]dingo.Module{
 		new(todo.Module),
+		new(projectgraphql.Module),
 		new(requestlogger.Module),
 	})
 }
